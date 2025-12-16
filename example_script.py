eb2f6037632d7b8ebee721fe6382cf461ca28b22
Marie Roald <roald.marie@gmail.com>
2025-12-16 10:58:47 +0100

    Update example script

--- example_script.py
+++ example_script.py
@@ -1,14 +1,15 @@
 import turtlethread
 
 needle = turtlethread.Turtle()
-with needle.triple_stitch(20):
-    for square in range(8):
-        for side in range(4):
-            needle.forward(200)
-            needle.right(90)
-        needle.right(45)
+with needle.running_stitch(20):
+    needle.forward(200)
+    needle.right(120)
+    needle.forward(200)
+    needle.right(120)
+    needle.forward(200)
+    needle.right(120)
 
 needle.show_info()
-needle.save("square_flower.jef")
+needle.save("triangle.jef")
 needle.visualise()
-needle.save("square_flower.svg")
+needle.save("triangle.svg")
